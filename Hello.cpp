ba8158bfdf6f3a3a360af9b4bdafb946e8c6eab2
Lauren Garwood <ls_garwood@yahoo.co.uk>
2023-02-20 14:29:10 +0000

    Hello.cpp intro

---
+++ Hello.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+int main() {
+  
+  std::cout << "Hello World!\n";
+  // will print Hello World!
+
+  std::cout << "       1\n";
+  std::cout << "     2 3\n";
+  std::cout << "   4 5 6\n";
+  std::cout << "7 8 9 10\n";
+  // Will print the numbers as it seems in a triangle
+
+}
+
+// C++ programmes are stored in files which usually have the file extension .cpp, which simply stands for "C plus plus"
+
+// C++, like most programming languages, runs line by line, from top to bottom. 
+
+// C++ programme structure..
+// Include libraries
+// The main() function
+// Beginning of function {
+// End of function }
+
+// std::cout is the “character output stream”. It is pronounced “see-out”.
+// << is an operator that comes right after it.
+// "Hello World!\n" is what’s being outputted here. You need double quotes around text. The \n is a special character that indicates a new line.
+// ; is a punctuation that tells the computer that you are at the end of a statement. It is similar to a period in a sentence.
+
+
+
