e3bd30e244813224fa0e899f255454ecb378c4e2
Lauren Garwood <ls_garwood@yahoo.co.uk>
2023-02-25 19:39:12 +0000

    Functoins Scope & Flexibility 2

--- FunctionsScopeFlexibility.cpp
+++ FunctionsScopeFlexibility.cpp
@@ -175,6 +175,33 @@ AAA AAA
 444 444
 */
 
+/*
+Function Templates
+
+Unlike regular functions, templates are entirely created in header files.
+
+Templates let us choose the type implementation right when you call the function. The type we choose may apply to the return type, a parameter type, or both.
+
+Here’s how we could build a template for print_cat_ears() so that the parameter type is flexible:
+
+template <typename T>
+void print_cat_ears(T item) {
+ 
+  std::cout << " " << item << "   " << item << " " << "\n";
+  std::cout << item << item << item << " " << item << item << item << "\n";
+ 
+}
+We can call the function for int, char, std::string, or double:
+
+print_cat_ears(2);
+ 
+// the output:
+//  2   2
+// 222 222
+Now we can use print_cat_ears() with a parameter of any type we want, as long as the type can be used with the methods expected. For example, we couldn’t pass an std::vector into the current print_cat_ears() because << won’t work with std::vector.
+
+Note: Using templates will slow down the program’s compile time, but speed up the execution time.
+*/
 
 
 
@@ -209,6 +236,14 @@ double average(double num1, double num2);
 int tenth_power(int num);
 bool is_palindrome(std::string text);
 
+// template
+template <typename T>
+T get_smallest(T num1, T num2) {
+  
+  return num2 < num1? num2 : num1;
+  
+}
+
 
 /* And libraries, headers and main in main.cpp
 #include <iostream>
@@ -220,5 +255,9 @@ int main() {
   std::cout << is_palindrome("racecar") << "\n";
   std::cout << tenth_power(3) << "\n";
   std::cout << average(8.0, 19.0) << "\n";
+  
+  // template
+  std::cout << get_smallest(100, 60) << "\n";
+  std::cout << get_smallest(2543.2, 3254.3) << "\n";
   
 }
